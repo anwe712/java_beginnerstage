720eb746bb5de6b7ac28a35f7f442ddfc1ab6a8d
Anwesha Ghosh <111485825+aviwesha272@users.noreply.github.com>
2023-04-22 18:20:38 +0530

    Update printNumber.java

--- printNumber.java
+++ printNumber.java
@@ -6,13 +6,13 @@ public class printNumber {
             int num = sc.nextInt();
 
             int i , count = 0 ;
-
+          //to get the number of factors 
             for (i = 1;i <= num ; i++){
                 if (num % i == 0)
                 {
                     count++;
                 }
-            }
+            } //if factor count is 2 then its a prime number
             if ( count == 2 ){
                 System.out.println("The number is a prime number");
 
